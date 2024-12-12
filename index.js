fabdc2318c48dc95e5d96f6f903adeadeb4bbcb5
Nicholas <njbroussard.1@gmail.com>
2024-12-11 23:05:06 -0600

    hardcode genres, install express

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const http = require('http')
 
+let genres = [
+  {
+    id: "1",
+    content: "Jazz",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Blues",
+    important: false
+  },
+  {
+    id: "3",
+    content: "Rock",
+    important: true
+  }
+]
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end(JSON.stringify(genres))
 })
 
-const PORT = 3001
+const PORT = 3002
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
