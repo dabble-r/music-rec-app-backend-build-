bb2168e3e8c4df0b92f6d1fa699ddccd80594dba
Nicholas <njbroussard.1@gmail.com>
2024-12-12 20:31:33 -0600

    install postman/Rest, add GET/POST/DELETE requests

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+
 
 let albums = [
   {
@@ -38,6 +40,41 @@ app.get('/api/albums/:id', (request, response) => {
     response.status(400).send('Not found!');
   }
 })
+
+app.delete('/api/albums/:id', (request, response) => {
+  const id = request.params.id
+  albums = albums.filter(album => album.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/albums', (request, response) => {
+  const generateId = () => {
+    const maxId = albums.length > 0
+      ? Math.max(...albums.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+
+  const body = request.body
+
+  if (!body.genre) {
+    return response.status(400).json({ 
+      error: 'genre missing' 
+    })
+  }
+
+  const album = {
+    genre: body.genre,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  albums = albums.concat(album)
+
+  response.json(album)
+})
+
 
 const PORT = 3002
 app.listen(PORT, () => {
