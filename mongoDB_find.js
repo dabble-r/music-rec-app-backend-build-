05ffed0c2c1e98b2ab71edd31557adb60cb12bda
Nicholas <njbroussard.1@gmail.com>
2024-12-15 08:59:09 -0600

    create new/find query mongoDB

--- mongoDB_find.js
+++ mongoDB_find.js
@@ -1,8 +1,9 @@
 // Import Mongoose
 const mongoose = require('mongoose');
 
+// code to connect to db from MongoDB site
 // Encode password for connection string
-const password = encodeURIComponent('BiC3c6ahTdOjxNvz');
+const password = encodeURIComponent('Su41HrJmtSl8TUpo');
 
 // MongoDB Atlas connection URI
 const uri = `mongodb+srv://njb_admin:${password}@test1.sdbe3.mongodb.net/Test1?retryWrites=true&w=majority`;
@@ -12,7 +13,7 @@ mongoose.connect(uri)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('Connection error:', err));
 
-// Define a Schema for the Album collection
+  // Define a Schema for the Album collection
 const albumSchema = new mongoose.Schema({
   album: String,
   artist: String,
@@ -23,17 +24,10 @@ const albumSchema = new mongoose.Schema({
 // Create a Model from the Schema
 const Album = mongoose.model('Album', albumSchema);
 
-// Create and save a new album document
-const album = new Album({
-  album: 'Texas Flood',
-  artist: 'Stevie Ray Vaughan',
-  genre: 'Blues/Rock',
-  important: true,
-});
-
-album.save()
-  .then(() => {
-    console.log('Album saved!');
-    mongoose.connection.close(); // Close the connection after saving
+// Iterate through DB and find any/all notes(s)
+Album.find({}).then(result => {
+  result.forEach(album => {
+    console.log(album)
   })
-  .catch(err => console.error('Error saving album:', err));
+  mongoose.connection.close()
+})
---
+++ mongoDB_new.js
@@ -0,0 +1,43 @@
+// Import Mongoose
+const mongoose = require('mongoose');
+
+// code to connect to db from MongoDB site
+// Encode password for connection string
+const password = encodeURIComponent('Su41HrJmtSl8TUpo');
+
+// MongoDB Atlas connection URI
+const uri = `mongodb+srv://njb_admin:${password}@test1.sdbe3.mongodb.net/Test1?retryWrites=true&w=majority`;
+
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Connection error:', err));
+
+// Define a Schema for the Album collection
+const albumSchema = new mongoose.Schema({
+  album: String,
+  artist: String,
+  genre: String,
+  important: Boolean,
+});
+
+// Create a Model from the Schema
+const Album = mongoose.model('Album', albumSchema);
+
+// Create and save a new album document
+const album = new Album({
+  album: 'One Size Fits All',
+  artist: 'Frank Zappa',
+  genre: 'Rock',
+  important: true,
+});
+
+album.save()
+  .then(() => {
+    console.log('Album saved!');
+    mongoose.connection.close(); // Close the connection after saving
+  })
+  .catch(err => console.error('Error saving album:', err));
+  
+
+  
